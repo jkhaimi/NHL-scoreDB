f290e21d3de6553631dce36c8f5743bc56d5c007
jkhaimi <jesse.haimi@icloud.com>
2025-01-26 16:21:54 +0200

    pelaajien lisays aloitettu

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,12 @@ const PORT = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-const filePath = "./games.json";
+const gamesFilePath = "./games.json";
+const playersFilePath = "./players.json";
 
 // API: Hae kaikki tulokset
 app.get("/api/games", (req, res) => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
+  fs.readFile(gamesFilePath, "utf-8", (err, data) => {
     if (err) {
       return res.status(500).json({ error: "Tiedoston lukeminen epäonnistui" });
     }
@@ -40,6 +41,39 @@ app.post("/api/games", (req, res) => {
     });
   });
 });
+
+// API: Hae kaikki pelaajat
+app.get("/api/players", (req, res) => {
+  fs.readFile(playersFilePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Tiedoston lukeminen epäonnistui" });
+    }
+    const players = JSON.parse(data || "[]");
+    res.json(players);
+  });
+});
+
+app.post("/api/players", (req, res) => {
+  const newPlayer = req.body;
+
+  fs.readFile(playersFilePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Tiedoston lukeminen epäonnistui" });
+    }
+
+    const players = JSON.parse(data || "[]");
+
+    players.push(newPlayer);
+
+    fs.writeFile(playersFilePath, JSON.stringify(players, null, 2), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Tiedoston tallentaminen epäonnistui" });
+      }
+      res.status(201).json(newPlayer);
+    })
+  })
+})
+
 
 // Käynnistä palvelin
 app.listen(PORT, () => {
